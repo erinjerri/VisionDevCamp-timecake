f1c6db46c686f2dc41712ea4e54c315386916aaf
spease <peasteven@gmail.com>
2024-03-31 12:28:30 -0700

    Sunday changes

--- TimeCake/ContentView.swift
+++ TimeCake/ContentView.swift
@@ -9,15 +9,10 @@ import SceneKit
 import SwiftUI
 import RealityKit
 
-var selectedEntity: Mode = .work
-
-enum Mode {
-    case work, play, sleep
-}
-
 func buildCylinder(color: UIColor, secondsElapsed: UInt64, secondsTotal: UInt64) -> Entity {
     let heightPerSecond: Double = 0.000002
     let height = heightPerSecond * Double(secondsTotal)
+    let height_2 = Float(height/2.0)
     let transparentHeight = Float((Double(secondsElapsed) / Double(secondsTotal)) * height)
     let cylinderHeight = Float(height) - transparentHeight
     let radius = Float(0.05)
@@ -25,9 +20,19 @@ func buildCylinder(color: UIColor, secondsElapsed: UInt64, secondsTotal: UInt64)
     let anchorEntity = Entity()
     
     let transparent = ModelEntity(mesh: .generateCylinder(height: transparentHeight, radius: radius), materials: [SimpleMaterial(color: color.withAlphaComponent(0.5), roughness: 0.5, isMetallic: false)])
-    transparent.transform.translation = simd_float3(x: 0.0, y: cylinderHeight / 2.0, z: -0.2)
+    transparent.transform.translation = simd_float3(x: 0.0, y: height_2 - transparentHeight/2.0, z: -0.2)
     let cylinderEntity = ModelEntity(mesh: .generateCylinder(height: cylinderHeight, radius: radius), materials: [SimpleMaterial(color: color, roughness: 0.0, isMetallic: false)])
-    cylinderEntity.transform.translation = simd_float3(x: 0.0, y: -cylinderHeight / 2.0, z: -0.2)
+    cylinderEntity.transform.translation = simd_float3(x: 0.0, y: cylinderHeight / 2.0 - height_2, z: -0.2)
+    cylinderEntity.components.set(
+        CollisionComponent(
+            shapes: [.generateBox(size: [radius*2,Float(cylinderHeight), radius*2])],
+              mode: .default,
+              filter: .default
+        )
+    )
+    cylinderEntity.components.set(HoverEffectComponent())
+    cylinderEntity.components.set(InputTargetComponent())
+    
     anchorEntity.addChild(transparent)
     anchorEntity.addChild(cylinderEntity)
     anchorEntity.components.set(
@@ -43,14 +48,25 @@ func buildCylinder(color: UIColor, secondsElapsed: UInt64, secondsTotal: UInt64)
     return anchorEntity
 }
 
-struct ModeData {
+class ModeData {
     var secondsTotal: UInt64
     @State var secondsElapsed: UInt64 = 0
+    var timeRemaining: String {
+        get {
+            let secondsLeft = self.secondsTotal - self.secondsElapsed
+            if secondsLeft > 3600 {
+                return String(format: "%.1f", Float(secondsLeft) / 3600.0)
+            }
+            return String(secondsLeft / 60)
+        }
+    }
     
-    func secondsRemaining() -> UInt64 {
-        self.secondsTotal - self.secondsElapsed
+    init(secondsTotal: UInt64) {
+        self.secondsTotal = secondsTotal
     }
 }
+
+private var selectedEntity: ModeData! = nil
 
 struct ContentView: View {
     @State private var work: ModeData = ModeData(secondsTotal: 36000)
@@ -61,39 +77,22 @@ struct ContentView: View {
         HStack {
             buildCake()
             VStack {
-                HStack {
-                    Text(String(work.secondsRemaining()))
-                        .padding()
-                        .background(.red)
-                        .font(.largeTitle)
-                        .clipShape(Capsule())
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack {
-                    Text(String(play.secondsRemaining()))
-                        .padding()
-                        .background(.green)
-                        .font(.largeTitle)
-                        .clipShape(Capsule())
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack {
-                    /*
-                    RealityView { content in
-                        content.add(buildCylinder(color: .blue, secondsElapsed: 18000, secondsTotal: 36000))
-                    }
-                    .padding(0)
-                     */
-                    Text(String(sleep.secondsRemaining()))
-                        .padding()
-                        .background(.blue)
-                        .font(.largeTitle)
-                        .clipShape(Capsule())
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                buildButton(mode: self.work, color: .red)
+                buildButton(mode: self.play, color: .green)
+                buildButton(mode: self.sleep, color: .blue)
             }
         }
         .padding()
+    }
+    
+    func buildButton(mode: ModeData, color: some ShapeStyle) -> some View {
+        Button(mode.timeRemaining, action: {
+            print("BUTTON \(mode)!!")
+            selectedEntity = mode
+        })
+        .font(.largeTitle)
+        .tint(color)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
     
     func buildCake() -> some View {
@@ -102,12 +101,12 @@ struct ContentView: View {
                 content.add(buildCylinder(color: .red, secondsElapsed: work.secondsElapsed, secondsTotal: work.secondsTotal))
             }
             .padding(0)
-            .gesture(TapGesture()
+            .gesture(SpatialTapGesture()
                 .targetedToAnyEntity()
-                .onEnded({ value in
+                .onEnded({ _ in
                     print("WORK!!!")
-                    selectedEntity = .work
-                    work.secondsElapsed += 1
+                    selectedEntity = work
+                    selectedEntity.secondsElapsed += 1000
             }))
             RealityView { content in
                 content.add(buildCylinder(color: .green, secondsElapsed: play.secondsElapsed, secondsTotal: play.secondsTotal))
@@ -116,7 +115,7 @@ struct ContentView: View {
             .gesture(TapGesture()
                 .targetedToAnyEntity()
                 .onEnded({ value in
-                    selectedEntity = .play
+                    selectedEntity = play
             }))
             RealityView { content in
                 content.add(buildCylinder(color: .blue, secondsElapsed: sleep.secondsElapsed, secondsTotal: sleep.secondsTotal))
@@ -124,7 +123,7 @@ struct ContentView: View {
             .gesture(TapGesture()
                 .targetedToAnyEntity()
                 .onEnded({ value in
-                    selectedEntity = .sleep
+                    selectedEntity = sleep
             }))
             .padding(0)
         }
